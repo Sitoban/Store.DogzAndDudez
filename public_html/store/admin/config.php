64a33e7083ba95e3110cae9f2646c3504ad1e6f4
sitoban <sitoban@sg2plcpnl0129.prod.sin2.secureserver.net>
2017-05-06 09:22:55 -0700

    Initial commit

---
+++ public_html/store/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://store.dogzanddudez.com/admin/');
+define('HTTP_CATALOG', 'http://store.dogzanddudez.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://store.dogzanddudez.com/admin/');
+define('HTTPS_CATALOG', 'http://store.dogzanddudez.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/sitoban/public_html/store/admin/');
+define('DIR_SYSTEM', '/home/sitoban/public_html/store/system/');
+define('DIR_LANGUAGE', '/home/sitoban/public_html/store/admin/language/');
+define('DIR_TEMPLATE', '/home/sitoban/public_html/store/admin/view/template/');
+define('DIR_CONFIG', '/home/sitoban/public_html/store/system/config/');
+define('DIR_IMAGE', '/home/sitoban/public_html/store/image/');
+define('DIR_CACHE', '/home/sitoban/public_html/store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/sitoban/public_html/store/system/storage/download/');
+define('DIR_LOGS', '/home/sitoban/public_html/store/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/sitoban/public_html/store/system/storage/modification/');
+define('DIR_UPLOAD', '/home/sitoban/public_html/store/system/storage/upload/');
+define('DIR_CATALOG', '/home/sitoban/public_html/store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'sitobandb');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'dogzanddudezstore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ public_html/store/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://store.dogzanddudez.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://store.dogzanddudez.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/sitoban/public_html/store/catalog/');
+define('DIR_SYSTEM', '/home/sitoban/public_html/store/system/');
+define('DIR_LANGUAGE', '/home/sitoban/public_html/store/catalog/language/');
+define('DIR_TEMPLATE', '/home/sitoban/public_html/store/catalog/view/theme/');
+define('DIR_CONFIG', '/home/sitoban/public_html/store/system/config/');
+define('DIR_IMAGE', '/home/sitoban/public_html/store/image/');
+define('DIR_CACHE', '/home/sitoban/public_html/store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/sitoban/public_html/store/system/storage/download/');
+define('DIR_LOGS', '/home/sitoban/public_html/store/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/sitoban/public_html/store/system/storage/modification/');
+define('DIR_UPLOAD', '/home/sitoban/public_html/store/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'sitobandb');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'dogzanddudezstore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
